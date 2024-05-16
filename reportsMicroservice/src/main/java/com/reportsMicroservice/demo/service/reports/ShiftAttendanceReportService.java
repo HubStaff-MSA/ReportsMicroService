691979f694add5628277e5734d2fc99fb1452cc3
Maryam Sherif <mariamsherif2.ms@gmail.com>
2024-05-16 20:54:36 +0300

    commented shift reports

---
+++ reportsMicroservice/src/main/java/com/reportsMicroservice/demo/service/reports/ShiftAttendanceReportService.java
@@ -0,0 +1,13 @@
+package com.reportsMicroservice.demo.service.reports;
+
+import com.reportsMicroservice.demo.model.others.Project;
+import com.reportsMicroservice.demo.model.reports.ProjectBudgetsReport;
+import com.reportsMicroservice.demo.model.reports.ShiftAttendanceReport;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShiftAttendanceReportService {
+
+
+}
