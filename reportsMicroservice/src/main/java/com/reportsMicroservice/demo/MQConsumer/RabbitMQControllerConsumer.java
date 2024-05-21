5b8932923bb05cba78a057816c187adb98643c2c
Maryam Sherif <mariamsherif2.ms@gmail.com>
2024-05-21 22:15:22 +0300

    commented consumer

--- reportsMicroservice/src/main/java/com/reportsMicroservice/demo/MQConsumer/RabbitMQControllerConsumer.java
+++ reportsMicroservice/src/main/java/com/reportsMicroservice/demo/MQConsumer/RabbitMQControllerConsumer.java
@@ -1,19 +1,19 @@
-package com.reportsMicroservice.demo.MQConsumer;
-
-import com.reportsMicroservice.demo.MQPublisher.RabbitMQServicePublisher;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/api")
-public class RabbitMQControllerConsumer {
-
-    private final RabbitMQServicePublisher rabbitMQService;
-
-    @Autowired
-    public RabbitMQControllerConsumer(RabbitMQServicePublisher rabbitMQService) {
-        this.rabbitMQService = rabbitMQService;
-    }
-
-}
+//package com.reportsMicroservice.demo.MQConsumer;
+//
+//import com.reportsMicroservice.demo.MQPublisher.RabbitMQServicePublisher;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/api")
+//public class RabbitMQControllerConsumer {
+//
+//    private final RabbitMQServicePublisher rabbitMQService;
+//
+//    @Autowired
+//    public RabbitMQControllerConsumer(RabbitMQServicePublisher rabbitMQService) {
+//        this.rabbitMQService = rabbitMQService;
+//    }
+//
+//}
