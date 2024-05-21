52fb6e1a07085f85f589787f3e00d6d40bc5d1b7
SaraShKamal <sara.hassan@student.guc.edu.eg.com>
2024-05-21 22:51:17 +0300

    Commit

--- reportsMicroservice/src/main/java/com/reportsMicroservice/demo/MQPublisher/RabbitMQControllerPublisher.java
+++ reportsMicroservice/src/main/java/com/reportsMicroservice/demo/MQPublisher/RabbitMQControllerPublisher.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class RabbitMQControllerPublisher {
 
+
     private final RabbitMQServicePublisher rabbitMQService;
 
     @Autowired
